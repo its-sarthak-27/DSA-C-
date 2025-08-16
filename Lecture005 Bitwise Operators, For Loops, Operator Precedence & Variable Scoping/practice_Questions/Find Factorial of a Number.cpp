3cc0dad24c76309e4ae0bd7f14b44efd7c11469d
SARTHAK KUMAR SRIVASTAV <sarthakinsight@gmail.com>
2025-08-16 14:13:13 +0530

    Find Factorial of a Number.cpp

---
+++ Lecture005 Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/practice_Questions/Find Factorial of a Number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long long fact = 1;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Factorial not defined for negative numbers.";
+    else {
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        cout << "Factorial = " << fact << endl;
+    }
+    return 0;
+}
